5183570d03817d11ce37ee7aae6499fe93c5b99f
omprakash <95085498+Silentdotexe@users.noreply.github.com>
2021-12-08 01:46:36 +0530

    Add files via upload
    List read write execute permissions of a file in current directory for owner

---
+++ rwx_file_perm_cur_dir.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+f=$(find . -perm 0700 -type f)
+echo "RWX files in current directory ( only OWNER)"
+echo -e "-----------------------------------------------"
+for i in $f
+do
+    echo -e "\n$i"
+done
+#author /OmPrakash/slientdotexe
